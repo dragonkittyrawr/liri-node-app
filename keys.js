4cd2810ab4c0d6d4d12c3acc149d84d5c09b8c50
Erin Stewart <dragonkittyrawr@users.noreply.github.com>
2017-04-10 09:59:13 -0500

    Removed key.js.

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log("KEYS LOADED");
-
-exports.twitterKeys = {
-  consumer_key: "DLGZ4x51IsupLLQy6VN6trhwE",
-  consumer_secret: "7p091FbdqJt0X8LxBhe5BXbLa6hDKyLY4ryNo5jhSiWeIfqLlF",
-  access_token_key: "46385425-GUuWHz8G1XjEMGyftCFnQMCHerQrYYKtGHTS3uV86",
-  access_token_secret: "nzwRsL9RhNedaA2J3A4uXCCJ4yFUJf90TJVyrGw3lFCEx"
-}
